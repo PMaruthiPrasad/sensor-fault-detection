310f46d56c16324c23473921801fb620d486146b
Maruthi Prasad <pmaruthiprasad16@gmail.com>
2023-11-13 06:13:14 +0530

    folders structure  added

--- data_dump.py
+++ data_dump.py
@@ -10,13 +10,13 @@ COLLECTION_NAME='sensor2'
 
 if __name__ == '__main__':
     df=pd.read_csv("aps_failure_training_set1.csv")
-    print(f"ROWS and columns: {df.shape}")
+    #print(f"ROWS and columns: {df.shape}")
 
 # Convert data from to Json s can dump these records in mongodb
 
     df.reset_index(drop=True,inplace=True)
     json_record=list(json.loads(df.T.to_json()).values())
-    print(json_record[0])
+    #print(json_record[0])
 
 
 # Insert converted record to mongodb 
--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import find_packages,setup
 from typing import List
 
+REQUIREMENT_FILE_NAME = 'requirements.txt'
+HYPHEN_E_DOT = "-e ."
+
 def get_requirements()->List[str]:
     """ 
     this function will return list of requiremnst 
     """
-    # Assignment-1 
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
     """
     write code to read requiremnts.txt file and append requirements in requirmeent_list variable 
@@ -19,6 +29,6 @@ setup (
     author='Maruthi',
     author_email='pmaruthiprasad16@gmail.com',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=get_requirements(),
 
 )
