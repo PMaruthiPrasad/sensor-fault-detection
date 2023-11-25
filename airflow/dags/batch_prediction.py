9593d796fbaad003e862c77ba5683fdcb1e07de9
PMaruthiPrasad <93707618+PMaruthiPrasad@users.noreply.github.com>
2023-11-25 11:28:45 +0530

    Update batch_prediction.py

--- airflow/dags/batch_prediction.py
+++ airflow/dags/batch_prediction.py
@@ -57,4 +57,4 @@ with DAG(
 
     )
 
-    download_input_files >> generate_prediction_files >> upload_prediction_files
+    download_input_files >> generate_prediction_files >> upload_prediction_files   
